ca4c374d6a358d29dd5032f9708d4f26a89a90bc
Jonathan Rocha <jonathan.rocha@msn.com>
2018-10-08 20:31:53 -0300

    Interface 80% done

--- src/cljs/online_lenormand/views/home.cljs
+++ src/cljs/online_lenormand/views/home.cljs
@@ -1,26 +1,64 @@
 (ns online-lenormand.views.home
   (:require [reagent.core :as r]))
 
+;; Color util ==================================================================
+
+(defn random-color []
+  (let [random-number (rand-int 360)]
+    (if (and (> random-number 20) (< random-number 160))
+      (+ random-number 100)
+      random-number)))
+
+(def hue (random-color))
+
+(defn title-font-color [hue]
+  (let [saturation 100
+        lightness 95]
+    (str "hsl(" hue ", " saturation "%, " lightness "%)")))
+
+(defn description-font-color [hue]
+  (let [saturation 100
+        lightness 90]
+    (str "hsl(" hue ", " saturation "%, " lightness "%)")))
+
+(defn gradient-background [hue]
+  (let [saturation 80
+        lightness 75]
+    (str "linear-gradient(to top, hsl(" hue ", " saturation "%, " lightness "%), hsl(" hue ", " saturation "%, " (- lightness 5) "%))")))
+
+;; Components ==================================================================
+
 (defn title []
   (let [styles
-        {:title {:color "blue"}}]
+        {:title {:color (title-font-color hue)
+                 :font-family "Charmonman"
+                 :font-size "7vw"
+                 :font-weight "bolder"}}]
 
     [:h1 {:style (:title styles)}
       "Online Lenormand"]))
 
 (defn description []
   (let [styles
-        {:text {:color "red"}}]
+        {:text {:color (description-font-color hue)
+                :width "70%"
+                :margin-top ".08em"
+                :margin-bottom "2em"
+                :font-size "1.4vw"
+                :font-family "Lora"
+                :text-align "center"}}]
 
     [:p {:style (:text styles)}
-      "Lalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalalala"]))
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]))
 
 (defn prompt []
   (let [styles
-        {:prompt {:color "yellow"}}]
+        {:prompt {:color (title-font-color hue)
+                  :font-family "Charmonman"
+                  :font-size "3vw"}}]
 
     [:h1 {:style (:prompt styles)}
-      "What do you wanna know?"]))
+      "What do you want to reflect about?"]))
 
 (def input-atom (r/atom ""))
 
@@ -37,10 +75,9 @@
          :value @input-atom
          :on-change #(reset! input-atom (-> % .-target .-value))}]]))
 
-
 (defn home []
   (let [styles
-        {:container {:background-color "grey"
+        {:container {:background (gradient-background hue)
                      :height "100%"
                      :width "100%"
 
